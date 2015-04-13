a3f1a0116025eb52ade51b6a8bbeac410a06417c
Hana Stanojkovic <hanastanojkovic@gmail.com>
2015-04-13 15:23:14 -0400

    Problems with report function fixed

--- server/lib/report.js
+++ server/lib/report.js
@@ -2,9 +2,6 @@
 // mailing script
 var postmark = require("postmark")('f8536579-7284-4950-9e3d-977cc96332d0');
 module.exports = {
-
-	testMapUrl: "",
-
   testalert: {
   "name":"barak",
   "location": [1,2],
@@ -12,7 +9,6 @@ module.exports = {
   "time":"3:34PM",
   "contacts":["1234567890","1234567890","1234567890","1234567890","1234567890"] 
   },
-
 
 	report:function(data){
 		var reportObjectArray = [];
@@ -26,48 +22,23 @@ module.exports = {
 		
     return "This is the Report page";
 	},
-
-
-// receive a report through the request (POST request)
-// create an object from its data
-// -maybe- save the report to a file (if we choose to save reports) 
-// take the object and turn it into a string (and then later format it to make it pretty)
-// send it to email! (textbody)
-
-function  getGoogleMapLink(location) {
-	return "https://www.google.com/maps/place/" +"/@" +location[0].toString() +","+location[1].toString()+ ",18z/data=!4m2!3m1!1s0x0:0x0";
-
-
-}
-
-var coordinates = [40.711365, -74.005132]
-module.exports.testMapUrl = getGoogleMapLink(coordinates);
-
-
-
 	alert:function(data){
     console.log(data);
   }
 };
 
 function email(reportObjectArray){
-
     postmark.send({
       "From": "bm09148n@pace.edu",
       "To": "hanastanojkovic@gmail.com",
       "Subject": ":)",
       "TextBody": ''+reportObjectArray+'',
       "Tag": "important"
-    },
-    
-    function(error, success){    
+    }, function(error, success){
       if(error){
         console.error("Unable to send via postmark: " + error.message);
         return;
       }
-     
       console.info("Sent to postmark for delivery")
-    
     });
 }
-
