da8d3c2f25d1d411fb16e731344c75bb873d3d5a
Hana Stanojkovic <hanastanojkovic@gmail.com>
2015-04-14 17:23:45 -0400

    Readded getGoogleMapLink function and started developing GET request for Pace whitepages to later retrieve student info for alert function

--- server/lib/report.js
+++ server/lib/report.js
@@ -2,9 +2,11 @@
 // mailing script
 var postmark = require("postmark")('f8536579-7284-4950-9e3d-977cc96332d0');
 module.exports = {
+    
+  // below is temporary alert
     testalert: {
-        "name": "Barak",
-        "location": [1, 2],
+        "name": "hs20139n",
+        "location": [40.7134519,-74.003797],
         "date": "04/09/2015",
         "time": "3:34PM",
         "contacts": ["1234567890", "1234567890", "1234567890", "1234567890", "1234567890"]
@@ -12,42 +14,53 @@ module.exports = {
 
     report: function(data) {
         // assume data received will be formatted in this order.
-        //{"name":"barak","date":"Jun 11th 2013","time":"3:12 PM","report":"blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah"}  
+        //{"name":"barak","date":"Jun 11th 2013","time":"3:12 PM","report":"blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah"}  
+        var subjectLine = "Sexual Harrassment Report!"
 
         var reportObjectArray = [];
 
         for (var key in data) {
             reportObjectArray.push(data[key].toString());
+            // try formatting here 
         }
 
-        var emailTextBody = "------" + "\n" + "REPORT" + "\n\n" + "------" + "\n" + "Name: " + reportObjectArray[0] + "\n" + "Date of Incident: " + reportObjectArray[1] + "\n" + "Time of Incident: " + reportObjectArray[2] + "\n\n\n" + "Report: " + reportObjectArray[3];
+        var emailTextBody = "Name: " + reportObjectArray[0] + "\n" + "Date of Incident: " + reportObjectArray[1] + "\n" + "Time of Incident: " + reportObjectArray[2] + "\n\n" + reportObjectArray[3];
 
-        email(emailTextBody);
+        email(subjectLine, emailTextBody);
 
         return "This is the Report page";
     },
     alert: function(data) {
-        // assume data received will be formatted in this order.
+
+        // assume data received will be formatted in the order of "testalert"
+
+        var subjectLine = "Sexual Harrassment Alert!"
 
         var alertObjectArray = [];
         for (var key in data) {
-            alertObjectArray.push(data[key].toString());
+            alertObjectArray.push(data[key]);
         }
 
-        var alertData = "------" + "\n" + "ALERT!" + "\n" + "------" + "\n" + alertObjectArray[0] + " is in trouble!" + "\n" + "Alert sent from this location: " + alertObjectArray[1] + "\n" + "at " + alertObjectArray[3] + " on " + alertObjectArray[2] + "\n" + "If you can, try to reach out to " + alertObjectArray[0] + " or alert the authorities.";
+        var alertData = alertObjectArray[0] + " is in trouble!" + "\n" + "Alert sent from this location: " + getGoogleMapLink(alertObjectArray[1]) + "\n" + "at " + alertObjectArray[3] + " on " + alertObjectArray[2] + "\n" + "If you can, try to reach out to " + alertObjectArray[0] + " or alert the authorities.";
 
-        console.log(alertData)
+
+        email(subjectLine, alertData);
 
         // because SMS is not set up, this formatted data currently has no where to go. 
         return alertData;
     }
 };
+function  getGoogleMapLink(location) {
+      
+        return "https://www.google.com/maps/place/" +"/@" +location[0].toString() +","+location[1].toString()+ ",18z/data=!4m2!3m1!1s0x0:0x0";
 
-function email(emailTextBody) {
+}
+
+function email(emailTextSubject, emailTextBody) {
     postmark.send({
         "From": "bm09148n@pace.edu",
         "To": "hanastanojkovic@gmail.com",
-        "Subject": "Sexual Harassment Report!",
+        "Subject": ''+emailTextSubject+'',
         "TextBody": '' + emailTextBody + '',
         "Tag": "Important"
     }, function(error, success) {
